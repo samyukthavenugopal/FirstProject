1c7142b40c7c363545c3ab5117b8f991c6cee5f0
SAMYUKTHA <71750386+samyukthavenugopal@users.noreply.github.com>
2020-09-27 21:13:39 +0530

    Addition code

--- pythonprog.py
+++ pythonprog.py
@@ -1,33 +1,4 @@
-print("Team No 18")
-print("Lets Create a Simple Caculator")
-# This function adds two numbers
-def add(x, y):
-    return x + y
+# Addition code from Samyuktha's Repository
+if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
 
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
-
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y
-
-
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-while True:
-    # Take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # Check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
