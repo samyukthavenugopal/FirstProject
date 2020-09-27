1528421bfe55ea21f4db963bf2206fb0e595605d
SAMYUKTHA <71750386+samyukthavenugopal@users.noreply.github.com>
2020-09-27 20:56:54 +0530

    code for selection

--- pythonprog.py
+++ pythonprog.py
@@ -1,2 +1,33 @@
 print("Team No 18")
 print("Lets Create a Simple Caculator")
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # Take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # Check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
